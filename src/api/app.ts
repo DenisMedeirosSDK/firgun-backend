efd041371c532e881f57a88fd088952fc514d480
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-11-19 20:13:06 -0300

    create server with express

---
+++ src/api/app.ts
@@ -0,0 +1,29 @@
+
+import cors from 'cors'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
+import helmet from 'helmet'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+
+app.get('/', (request: Request, response: Response) =>
+  response.end('hello world')
+)
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err}`
+  })
+})
+
+export { app }
---
+++ src/api/index.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config'
+
+import http from 'node:http'
+import { app } from './app'
+
+const PORT = process.env.PORT ?? 3000
+
+const server = http.createServer(app).listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
+process.on('uncaughtException', (error, origin) => {
+  console.log(`\n${origin} signal received. \n${error}`)
+})
+
+process.on('unhandledRejection', (error) => {
+  console.log(`\nunhandledRejection signal received. \n${error}`)
+})
+
+function gracefulShutdown (event: string) {
+  return (code: number) => {
+    console.log(`${event} received! with ${code}`)
+    server.close(() => {
+      console.log(`Closed express server with exit code ${code}`)
+      process.exit(code)
+    })
+  }
+}
+
+process.on('SIGINT', gracefulShutdown('SIGINT'))
+
+process.on('SIGTERM', gracefulShutdown('SIGTERM'))
+
+process.on('exit', (code) => {
+  console.log(`About to exit with code ${code}`)
+})
